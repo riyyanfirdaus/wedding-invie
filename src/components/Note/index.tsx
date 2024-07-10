ec37f563953e093f54662bc04aab1239be0f7ec6
riyyanfir <m.riyyan.firdaus@gmail.com>
2024-07-10 12:08:05 +0700

    feat: change font size presence love notes

--- src/components/Note/index.tsx
+++ src/components/Note/index.tsx
@@ -3,7 +3,7 @@ const Note = ({ data }: { data: Record<string, any> }) => {
     <div className="w-full shadow-md rounded-md p-3 space-y-2">
       <div className="flex justify-between items-center">
         <h2 className="font-bold text-accent">{data.name}</h2>
-        <span className={`${data.presence ? "bg-green-400" : "bg-red-400"} h-fit rounded-md text-xs text-primary px-3 py-1`}>{data.presence ? "Hadir" : "Tidak Hadir"}</span>
+        <span className={`${data.presence ? "bg-green-600" : "bg-red-600"} h-fit rounded-md text-[10px] text-primary px-3 py-1`}>{data.presence ? "Hadir" : "Tidak Hadir"}</span>
       </div>
       <p className="text-accent text-sm">{data.messages}</p>
     </div>
