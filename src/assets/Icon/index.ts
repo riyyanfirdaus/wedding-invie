c4ff6dc24b44b5ffd666550de0c08fcf6e470508
riyyanfir <m.riyyan.firdaus@gmail.com>
2024-08-07 16:06:10 +0700

    feat: add divider love icon

--- src/assets/Icon/index.ts
+++ src/assets/Icon/index.ts
@@ -3,3 +3,4 @@ export { default as BCALogo } from "./BCALogo";
 export { default as CreditCardChip } from "./CreditCardChip";
 export { default as LineBorderUp } from "./LineBorderUp";
 export { default as LineBorderDown } from "./LineBorderDown";
+export {default as DividerLove} from './DividerLove'
